3267d9b77ee118edca016731b8e5dc1cdfad74da
gyle <kyleclaudio@gmail.com>
2021-04-18 13:27:24 +1200

    add ceilling generation

--- Assets/Scripts/World/FloorGeneration.cs
+++ Assets/Scripts/World/FloorGeneration.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class FloorGeneration : MonoBehaviour
 {
     // ground generation 
-    public GameObject Ground; // ground
+    public GameObject Ground;
+    public GameObject Ceiling;
     public Transform PlayerLocation;
 
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("Collide");
         Instantiate(Ground, new Vector3(PlayerLocation.position.x + 10, -2.1f, -9.824199f), Quaternion.identity);
+        Instantiate(Ceiling, new Vector3(PlayerLocation.position.x + 10, 3.5f, -9.824199f), Quaternion.identity);
         WorldManager.Instance.SpawnNewObstacle();
         WorldManager.Instance.SpawnNewObstacle();
         WorldManager.Instance.SpawnNewObstacle();
