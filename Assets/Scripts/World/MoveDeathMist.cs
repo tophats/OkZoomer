523ec57ec15a538422c3b5ad69015b9a7059d18d
gyle <kyleclaudio@gmail.com>
2021-04-18 14:21:15 +1200

    add death mist

---
+++ Assets/Scripts/World/MoveDeathMist.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDeathMist : MonoBehaviour
+{
+    private Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(1 * Time.deltaTime, 0, 0);
+    }
+}
